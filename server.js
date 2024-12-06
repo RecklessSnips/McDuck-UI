881448a4a922d590e49321be5a576f66304b94be
RecklessSnips <lloyddonegan@gmail.com>
2024-12-05 23:14:51 -0500

    Fix

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
 const app = express();
 const port = process.env.PORT || 3000; // 使用 Railway 提供的 PORT 或本地开发的默认端口 3000
 
-// 假设 Vue 构建文件在 dist 目录
-app.use(express.static(path.join(__dirname, "dist")));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// 所有路由指向 SPA 入口文件
+app.use(express.static(join(__dirname, "dist"))); // 假设 Vue 构建文件在 dist 目录
+
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+  res.sendFile(join(__dirname, "dist", "index.html")); // SPA入口文件
 });
 
-// 启动服务器
 app.listen(port, "0.0.0.0", () => {});
